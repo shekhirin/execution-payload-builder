e8f1c61eda7d742f65335ee1abe290adeea7d4af
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-02-13 00:32:31 +0000

    Revert "use anytxenvelope (doesn't work)"
    This reverts commit 7049c4ade59db0bb02eac5ac7fed210eafc70c1c.

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,16 @@ use alloy::{
         Block as PrimitiveBlock, BlockBody, Header as PrimitiveHeader,
         Transaction as PrimitiveTransaction, TxEnvelope as EthTxEnvelope,
     },
-    network::AnyTxEnvelope,
-    rpc::types::{engine::ExecutionPayload, Block as RpcBlock, BlockTransactions},
+    eips::{eip2930::AccessList, eip7702::SignedAuthorization, Encodable2718, Typed2718},
+    primitives::{Bytes, ChainId, TxKind, B256, U256},
+    rpc::types::{
+        engine::ExecutionPayload, Block as RpcBlock, BlockTransactions,
+        Transaction as EthRpcTransaction,
+    },
 };
 use clap::Parser;
+use op_alloy::rpc_types::Transaction as OpRpcTransaction;
+use serde::{Deserialize, Serialize};
 
 /// Parses the given json file, creating an execution payload from it.
 #[derive(Parser, Debug)]
@@ -36,20 +42,173 @@ struct Args {
     raw: bool,
 }
 
-fn main() -> eyre::Result<()> {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+enum RpcTransaction {
+    Ethereum(EthRpcTransaction),
+    Optimism(OpRpcTransaction),
+}
+
+impl Typed2718 for RpcTransaction {
+    fn ty(&self) -> u8 {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.ty(),
+            RpcTransaction::Optimism(tx) => tx.ty(),
+        }
+    }
+}
+
+impl PrimitiveTransaction for RpcTransaction {
+    fn chain_id(&self) -> Option<ChainId> {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.chain_id(),
+            RpcTransaction::Optimism(tx) => tx.chain_id(),
+        }
+    }
+
+    fn nonce(&self) -> u64 {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.nonce(),
+            RpcTransaction::Optimism(tx) => tx.nonce(),
+        }
+    }
+
+    fn gas_limit(&self) -> u64 {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.gas_limit(),
+            RpcTransaction::Optimism(tx) => tx.gas_limit(),
+        }
+    }
+
+    fn gas_price(&self) -> Option<u128> {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.gas_price(),
+            RpcTransaction::Optimism(tx) => tx.gas_price(),
+        }
+    }
+
+    fn max_fee_per_gas(&self) -> u128 {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.max_fee_per_gas(),
+            RpcTransaction::Optimism(tx) => tx.max_fee_per_gas(),
+        }
+    }
+
+    fn max_priority_fee_per_gas(&self) -> Option<u128> {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.max_priority_fee_per_gas(),
+            RpcTransaction::Optimism(tx) => tx.max_priority_fee_per_gas(),
+        }
+    }
+
+    fn max_fee_per_blob_gas(&self) -> Option<u128> {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.max_fee_per_blob_gas(),
+            RpcTransaction::Optimism(tx) => tx.max_fee_per_blob_gas(),
+        }
+    }
+
+    fn priority_fee_or_price(&self) -> u128 {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.priority_fee_or_price(),
+            RpcTransaction::Optimism(tx) => tx.priority_fee_or_price(),
+        }
+    }
+
+    fn effective_gas_price(&self, base_fee: Option<u64>) -> u128 {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.effective_gas_price(base_fee),
+            RpcTransaction::Optimism(tx) => tx.effective_gas_price(base_fee),
+        }
+    }
+
+    fn is_dynamic_fee(&self) -> bool {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.is_dynamic_fee(),
+            RpcTransaction::Optimism(tx) => tx.is_dynamic_fee(),
+        }
+    }
+
+    fn kind(&self) -> TxKind {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.kind(),
+            RpcTransaction::Optimism(tx) => tx.kind(),
+        }
+    }
+
+    fn is_create(&self) -> bool {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.is_create(),
+            RpcTransaction::Optimism(tx) => tx.is_create(),
+        }
+    }
+
+    fn value(&self) -> U256 {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.value(),
+            RpcTransaction::Optimism(tx) => tx.value(),
+        }
+    }
+
+    fn input(&self) -> &Bytes {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.input(),
+            RpcTransaction::Optimism(tx) => tx.input(),
+        }
+    }
+
+    fn access_list(&self) -> Option<&AccessList> {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.access_list(),
+            RpcTransaction::Optimism(tx) => tx.access_list(),
+        }
+    }
+
+    fn blob_versioned_hashes(&self) -> Option<&[B256]> {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.blob_versioned_hashes(),
+            RpcTransaction::Optimism(tx) => tx.blob_versioned_hashes(),
+        }
+    }
+
+    fn authorization_list(&self) -> Option<&[SignedAuthorization]> {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.authorization_list(),
+            RpcTransaction::Optimism(tx) => tx.authorization_list(),
+        }
+    }
+}
+
+impl Encodable2718 for RpcTransaction {
+    fn encode_2718_len(&self) -> usize {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.inner.encode_2718_len(),
+            RpcTransaction::Optimism(tx) => tx.inner.inner.encode_2718_len(),
+        }
+    }
+
+    fn encode_2718(&self, out: &mut dyn alloy::primitives::bytes::BufMut) {
+        match self {
+            RpcTransaction::Ethereum(tx) => tx.inner.encode_2718(out),
+            RpcTransaction::Optimism(tx) => tx.inner.inner.encode_2718(out),
+        }
+    }
+}
+
+fn main() {
     let args = Args::parse();
 
     // read the file specified in `--path` otherwise read from stdin
     let block_json = if let Some(path) = &args.path {
-        std::fs::read_to_string(path)?
+        std::fs::read_to_string(path).unwrap()
     } else {
         let mut buffer = String::new();
-        std::io::stdin().read_to_string(&mut buffer)?;
+        std::io::stdin().read_to_string(&mut buffer).unwrap();
         buffer
     };
 
-    // parse the input
-    let block: RpcBlock<AnyTxEnvelope> = serde_json::from_str(&block_json)?;
+    // parse the file
+    let block: RpcBlock<RpcTransaction> = serde_json::from_str(&block_json).unwrap();
 
     // extract the parent beacon block root
     let parent_beacon_block_root = block.header.parent_beacon_block_root;
@@ -70,8 +229,10 @@ fn main() -> eyre::Result<()> {
     let blob_versioned_hashes = transactions
         .iter()
         .filter_map(|tx| {
-            if let AnyTxEnvelope::Ethereum(EthTxEnvelope::Eip4844(tx)) = tx {
-                return tx.tx().blob_versioned_hashes().map(|v| v.to_vec());
+            if let RpcTransaction::Ethereum(tx) = tx {
+                if let EthTxEnvelope::Eip4844(tx) = &tx.inner {
+                    return tx.tx().blob_versioned_hashes().map(|v| v.to_vec());
+                }
             }
             None
         })
@@ -93,30 +254,30 @@ fn main() -> eyre::Result<()> {
         execution_payload,
         blob_versioned_hashes,
         parent_beacon_block_root,
-    ))?;
+    ))
+    .unwrap();
 
+    // if raw is set, print the raw payload, without quotes
     if args.raw {
-        // if raw is set, print the raw payload
         println!("{json_request}");
-    } else {
-        // otherwise, construct the cast rpc command
-        let mut prefix = "cast rpc".to_string();
-        let suffix = format!("engine_newPayloadV3 --raw {json_request}");
-
-        if let Some(rpc_url) = args.rpc_url {
-            prefix += &format!(" --rpc-url {}", rpc_url);
-        }
-
-        if let Some(secret) = args.jwt_secret {
-            prefix += &format!(" --jwt-secret {}", secret);
-        }
-
-        // add the suffix and request
-        prefix += &format!(" {}", suffix);
-
-        // print the payload
-        println!("{prefix}");
+        return;
     }
 
-    Ok(())
+    // construct the cast rpc command
+    let mut prefix = "cast rpc".to_string();
+    let suffix = format!("engine_newPayloadV3 --raw {json_request}");
+
+    if let Some(rpc_url) = args.rpc_url {
+        prefix += &format!(" --rpc-url {}", rpc_url);
+    }
+
+    if let Some(secret) = args.jwt_secret {
+        prefix += &format!(" --jwt-secret {}", secret);
+    }
+
+    // add the suffix and request
+    prefix += &format!(" {}", suffix);
+
+    // print the payload
+    println!("{prefix}");
 }
